354104a32572317445005a749de02007113e28b2
SinaC <heymbeeck.joel@gmail.com>
2018-04-03 19:58:23 +0200

    TransactionManager started

---
+++ PPC.Module.Common/ITransactionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using PPC.Domain;
+
+namespace PPC.Module.Common
+{
+    public interface ITransationManager
+    {
+        event EventHandler<ShopTransaction> TransactionAdded;
+        event EventHandler<ShopTransaction> TransactionModified;
+        event EventHandler<ShopTransaction> TransactionDeleted;
+        event EventHandler TransactionReloaded;
+
+        IEnumerable<ShopTransaction> Transactions { get; }
+
+        void Reload(Session session);
+
+        void AddTransaction(ShopTransaction transaction);
+        void ModifyTransaction(ShopTransaction transaction);
+        void DeleteTransaction(ShopTransaction transaction);
+    }
+}
---
+++ PPC.Module.Common/TransactionManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyIoc;
+using PPC.Domain;
+using PPC.IDataAccess;
+using PPC.Log;
+
+namespace PPC.Module.Common
+{
+    /*
+    public class TransactionManager : ITransationManager
+    {
+        private ILog Logger => IocContainer.Default.Resolve<ILog>();
+        private ISessionDL SessionDL => IocContainer.Default.Resolve<ISessionDL>();
+
+        private List<ShopTransaction> _transactions;
+
+        #region ITransationManager
+
+        public event EventHandler<ShopTransaction> TransactionAdded;
+        public event EventHandler<ShopTransaction> TransactionModified;
+        public event EventHandler<ShopTransaction> TransactionDeleted;
+        public event EventHandler TransactionReloaded;
+
+        public IEnumerable<ShopTransaction> Transactions => _transactions;
+
+        public void Reload(Session session)
+        {
+            _transactions = session.Transactions;
+            TransactionReloaded?.Invoke(this, null);
+        }
+
+        public void AddTransaction(ShopTransaction transaction)
+        {
+            try
+            {
+                // Change locally
+                _transactions.Add(transaction);
+                // Persist modification
+                SessionDL.InsertTransaction(transaction);
+                // Raise event
+                TransactionAdded?.Invoke(this, transaction);
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex);
+            }
+        }
+
+        public void ModifyTransaction(ShopTransaction transaction)
+        {
+            try
+            {
+                // Change locally
+                _transactions.RemoveAll(x => x.Guid == transaction.Guid);
+                _transactions.Add(transaction);
+                // Persist modification
+                SessionDL.UpdateTransaction(transaction);
+                // Raise event
+                TransactionModified?.Invoke(this, transaction);
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex);
+            }
+        }
+
+        public void DeleteTransaction(ShopTransaction transaction)
+        {
+            try
+            {
+                // Change locally
+                _transactions.RemoveAll(x => x.Guid == transaction.Guid);
+                // Persist modification
+                SessionDL.DeleteTransaction(transaction);
+                // Raise event
+                TransactionDeleted?.Invoke(this, transaction);
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex);
+            }
+        }
+
+        #endregion
+
+        public TransactionManager()
+        {
+            _transactions = new List<ShopTransaction>();
+        }
+    }
+    */
+}
